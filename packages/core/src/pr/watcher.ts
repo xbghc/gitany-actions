22ba2d887c566169ae6a1ca1ba2316d2a51821bc
xbghc <1774733158@qq.com>
2025-09-11 12:47:45 +0800

    feat: 实现PR的open, close, merge事件监听

---
+++ packages/core/src/pr/watcher.ts
@@ -0,0 +1,60 @@
+import { GitcodeClient, PullRequest } from '@gitany/gitcode';
+
+interface WatchPullRequestOptions {
+  onClosed?: (pr: PullRequest) => void;
+  onOpen?: (pr: PullRequest) => void;
+  onMerged?: (pr: PullRequest) => void;
+}
+
+export function watchPullRequest(
+  client: GitcodeClient,
+  url: string,
+  options: WatchPullRequestOptions,
+) {
+  let prList: PullRequest[] = [];
+
+  const idExists = (id: number) => {
+    return prList.find((p) => p.id === id);
+  };
+
+  const stateChanged = (pr: PullRequest) => {
+    const existingPr = prList.find((p) => p.id === pr.id);
+    return existingPr && existingPr.state !== pr.state;
+  };
+
+  const check = async () => {
+    const newList = await client.pr.list(url, { state: 'all', page: 1, per_page: 10 });
+    for (const pr of newList) {
+      if (!idExists) {
+        triggerPullRequestEvent(pr, options);
+      }
+
+      if (stateChanged(pr)) {
+        triggerPullRequestEvent(pr, options);
+      }
+
+      break;
+    }
+
+    prList = newList;
+  };
+
+  const intervalId = setInterval(() => {
+    check();
+  }, 5000);
+
+  return () => {
+    clearInterval(intervalId);
+  };
+}
+
+export function triggerPullRequestEvent(pr: PullRequest, options: WatchPullRequestOptions) {
+  const { onClosed, onMerged, onOpen } = options;
+  if (onOpen && pr.state === 'open') {
+    onOpen(pr);
+  } else if (onClosed && pr.state === 'closed') {
+    onClosed(pr);
+  } else if (onMerged && pr.state === 'merged') {
+    onMerged(pr);
+  }
+}
