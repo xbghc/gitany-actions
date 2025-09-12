dd03f780b0500fc7f0daf65e8e3227bdd38c8be4
xbghc <1774733158@qq.com>
2025-09-12 15:56:51 +0800

    chore: 添加调试脚本

---
+++ scripts/dev-git.mjs
@@ -0,0 +1,23 @@
+import { GitClient } from '../packages/git-lib/dist/index.js';
+
+// const client = new GitClient('~/projects/MateChat');
+const client = new GitClient();
+
+// const result = await client.showFile('HEAD', 'package.json');
+// const result = await client.branch('test-branch', true);
+
+// --- test diff
+// const commit1 = '27f882661c587abcfabd327fe34d8ef71c2a3092';
+// const commit2 = '0a3b5c6b022c147655c375748ce88534d9bbc379';
+
+// const result = await client.diffCommits(commit1, commit2, {nameOnly: true, diffFilter: 'M'});
+
+// ------
+// test clone
+// const result = await client.clone('https://gitcode.com/xbghc/gitcode-demo.git', '~/projects/gitcode-demo2');
+
+// ------
+// test add
+// const result = await client.add('./packages/git-lib/*');
+
+console.log(result);
